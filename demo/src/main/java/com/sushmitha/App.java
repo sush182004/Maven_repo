4c46b8abd1f258326702a27b0efa35ff00759598
sush182004 <sushmitha182004@gmail.com>
2025-02-12 11:00:15 +0530

    RUNNING THE MAVEN PROJECT
    MAVEN PROJECT DEMO

---
+++ demo/src/main/java/com/sushmitha/App.java
@@ -0,0 +1,13 @@
+package com.sushmitha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
